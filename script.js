9e42bfeba99848837a44278dbeba881887559cdf
Asamaporn Bunmee <asama_b@outlook.com>
2021-02-14 18:59:17 +0700

    layout header and side bar !can't collapse subscribe button!

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("closebtn").style.fontSize = "25px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("closebtn").style.fontSize = "0";
+  }
